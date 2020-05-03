89452c8227f62db80ea0bd2f782fc729ecb7cb98
RICHHAPALCHOUDAHRY <51855961+RICHHAPALCHOUDAHRY@users.noreply.github.com>
2020-05-03 13:30:12 +0530

    Add files via upload

---
+++ guest.php
@@ -0,0 +1,407 @@
+
+<!DOCTYPE html>
+<html lang="en"> 
+<head >
+	<title>	INDIAN'S TOP HANDICRAFT</title>
+  <meta charset="utf-8">
+        <!-- wow movement start -->
+			<link  href="bootstrap/wow/css/libs/animate.css" rel="stylesheet"  />
+			 <script src="bootstrap/wow/dist/wow.min.js"></script>
+				<script>
+					new WOW().init();
+				</script>
+		<!-- wow movement end -->
+		<link href="bootstrap/fontawesome/css/all.css" rel="stylesheet">
+
+		 <!-- google fontfamily start -->
+			 <link href="https://fonts.googleapis.com/css?family=Courgette&display=swap" rel="stylesheet">
+			 <link href="https://fonts.googleapis.com/css?family=Satisfy&display=swap" rel="stylesheet">
+		<!-- google fontfamily end -->
+	
+
+
+<link  href="guest.css" rel="stylesheet"  type="text/css" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"  >
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" ></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+
+
+<meta  name="viewport" content="width=device-width,initial-scale=1"/>
+
+
+
+</head>
+
+
+
+
+<body>
+    
+
+  <!-- navigation start -->
+  <nav class="navbar navbar-expand-lg  navbar-dark bg  fixed-top">
+   
+           <a href="guest.php"class="navbar-brand"><img src="images/indian.png"></a>
+      
+  
+    
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+
+    <div class="collapse navbar-collapse " id="navbarColor01">
+            <ul class="navbar-nav ml-auto ">
+                   
+                   <li class="nav-item">
+                       <a class="nav-link" href="login.php">LOGIN</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="#aboutus">ABOUT US</a>
+                   </li>
+                   <li class="nav-item">
+                       <a class="nav-link" href="" target="_blank">OUR BLOG</a>
+                   </li>
+                   <!-- <li class="nav-item">
+                         <a class="nav-link" href="profile.php"><i class="fas fa-lg fa-user-circle"></i></a>
+                   </li>
+      
+                   <li class="nav-item">
+                      <a class="nav-link" href="#"><i class="fab fa-lg fa-opencart"></i></a>
+                   </li> -->
+             </ul>
+    
+  
+  </div> 
+</nav>
+<!-- navigation end -->
+
+
+<div class="container mt">
+    
+<div id="demo" class="carousel slide " data-ride="carousel">
+
+
+             <ul class="carousel-indicators">
+                 <li data-target="#demo" data-slide-to="0" class="active"></li>
+                 <li data-target="#demo" data-slide-to="1"></li>
+                 <li data-target="#demo" data-slide-to="2"></li>
+                 <li data-target="#demo" data-slide-to="3"></li>
+                 <li data-target="#demo" data-slide-to="4"></li>
+                 <li data-target="#demo" data-slide-to="5"></li>
+                 <li data-target="#demo" data-slide-to="6"></li>
+                 <li data-target="#demo" data-slide-to="7"></li>
+                 <li data-target="#demo" data-slide-to="8"></li>
+             </ul>
+
+               <div class="carousel-inner">
+                    <div class="carousel-item active">
+                          <img src="carousel/carousel.jpg"alt=""  >
+                        <!--  <div class="carousel-caption">
+                             <h1>Rajasthani-handicraft</h1>
+                             <button class="btn btn-success">learn more </button>
+                         </div> -->
+                    </div>
+                     <div class="carousel-item">
+                          <img src="carousel/carousel1.jpg" >
+                     </div>
+                      <div class="carousel-item">
+                                <img src="carousel/carousel2.jpg" >
+                      </div>
+                      <div class="carousel-item">
+                                <img src="carousel/carousel3.jpg" >
+                      </div>
+                      <div class="carousel-item">
+                                <img src="carousel/carousel4.jpg" >
+                      </div>
+                      <div class="carousel-item">
+                                <img src="carousel/carousel5.jpg" >
+                      </div>
+                      <div class="carousel-item">
+                                <img src="carousel/carousel6.jpg" >
+                      </div>
+                      <div class="carousel-item">
+                                <img src="carousel/carousel7.jpg" >
+                      </div>
+                      <div class="carousel-item">
+                                <img src="carousel/carousel8.jpg" >
+                      </div>
+               </div>
+
+                   <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                      <span class="carousel-control-prev-icon"></span>
+                   </a>
+                   <a class="carousel-control-next" href="#demo" data-slide="next">
+                       <span class="carousel-control-next-icon"></span>
+                   </a>
+
+</div>
+</div>
+
+<section>
+  <div class="container">
+      <div class="sectionbox1 text-center ">
+          <hr>
+           <h1>CATEGORIES</h1>
+          <hr>
+      </div>
+      <div class="sectionbox2 text-center">
+        <div class="row h-50 ">
+          <div class="col-lg-3 col-xl-3 bd col-md-6 col-sm-6 col-xs-12 bgimg1 pt-5">
+            <h5><a href="productguest.php" target="_blank"><span style="color:rgb(255, 255, 71,)!important;"> PASHMINA SHAWLS</span></a> </h5>
+            
+          </div>
+          <div class="col-lg-3 col-xl-3 bd col-md-6 col-sm-6 col-xs-12 bgimg2 pt-5">
+            <h5><a href="productguest.php" target="_blank"><span style="color: white!important;">WOOD WORK </span></a></h5> 
+          </div>
+          <div class="col-lg-3 col-xl-3 bd col-md-6 col-sm-6 col-xs-12 bgimg3 pt-5">
+            <h5> <a href="productguest.php"target="_blank"><span style="color: white!important;">POTTERY </span></a></h5>
+          </div>
+          <div class="col-lg-3 col-xl-3 bd col-md-6  col-sm-6 col-xs-12 bgimg4 pt-5">
+            <h5><a href="productguest.php"target="_blank"><span style="color: white!important;">LEATHER </span></a></h5>
+          </div>
+          
+        </div>
+        <div class="row h-50 ">
+          <div class="col-lg-3 col-xl-3  col-md-6 bd col-sm-6 col-xs-12 bgimg5 pt-5">
+            <h5><a href="productguest.php"target="_blank"><span style="color: white!important;">JUTE </span></a></h5>
+          </div>
+          <div class="col-lg-3 col-xl-3 col-md-6 bd  col-sm-6 col-xs-12 bgimg6 pt-5">
+            <h5><a href="productguest.php"target="_blank"><span style="color: white!important;">SHELL</a></span></h5>
+          </div>
+          <div class="col-lg-3 col-xl-3 col-md-6 bd col-sm-6 col-xs-12 bgimg7 pt-5">
+          <h5><a href="productguest.php"target="_blank"><span style="color: white!important;"><span style="color: white!important;">BRASS  HANDICRAFT </span></a></h5>
+          </div>
+          <div class="col-lg-3 col-xl-3 col-md-6 bd col-sm-6 col-xs-12 bgimg8 pt-5">
+            <h5><a href="productguest.php" target="_blank"><span style="color: white!important;">BAMBOO HANDICRAFT</span></a></h5>
+          </div>
+          
+        </div>
+      </div>
+
+      
+  </div>
+</section>
+<div class="container">
+  <div class="ourpartner text-center">
+    <hr>
+    <h1>
+      Our Partner
+    </h1>
+    <hr>
+  </div>
+  
+</div>
+<div class="container">
+    <div class="">
+      <div class="row company mt-1">
+        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+          <div class="companyimage1 ">
+            
+          </div>
+          <div class="companycontent1">
+            <p>
+              THE <i class="fab fa-2x fa-fedex"> </i> Corporation is an American multinational courier delivery services company headquartered in Memphis, Tennessee. The name "FedEx" is a syllabic abbreviation of the name of the company's original air division, Federal Express, which was used from 1973 until 2000.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+          <div class="companyimage2 ">
+            <a href=""></a>
+          </div>
+          <div class="companycontent2">
+            <p>
+             <i class="fab fa-2x fa-postnl"> </i>PostNL formerly TNT NV is a mail, parcel and e-commerce corporation with operations in the Netherlands, Germany, Italy, Belgium, and the United Kingdom. It provides universal delivery in the Netherlands, and is publicly listed at Euronext Amsterdam.
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+          <div class="companyimage3 ">
+            
+          </div>
+          <div class="companycontent3">
+            <p>
+          The <i class="fab fa-2x fa-dhl"> </i> International GmbH is an international courier, parcel, and express mail service which is a division of the German logistics company Deutsche Post DHL. Deutsche Post DHL is the world's largest logistics company, now in over 220 countries and territories worldwide, particularly in sea and air mail
+            </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 col-xs-12">
+          <div class="companyimage4 ">
+            
+          </div>
+          <div class="companycontent4">
+            <p>
+              China Ocean Shipping Company, known as COSCO, was a Chinese state-owned shipping and logistics services supplier company. Its headquarters is in Ocean Plaza in the Xicheng District in Beijing
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  
+  </div>
+
+
+<footer>
+    <div class="container" id="aboutus"> 
+      <div class="row ">
+        <div class="col-lg-3 text-center pt-3">
+          <div class="desktopshoplink">
+            <div class="desktoptitle  ">
+              <a href=""><h4>ONLINE SHOPING</h4></a>
+            </div>
+            <div class="link">
+              <a href="">brand1</a><br>
+              <a href="">brand2</a><br>
+              <a href="">brand2</a>
+
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-lg-3 text-center pt-3">
+            <div class="desktopshoplink">
+               <div class="desktoptitle">
+                <a href=""><h4>USEFUL LINKS</h4></a>
+                </div>
+              <div>
+                <a href="">CONTACT US</a><br>
+                <a href="">FAQ</a><br>
+                <a href="">T&C</a><br>
+                <a href="">TRACK ORDER</a><br>
+                <a href="">SHIPPING WAY</a><br>
+                <a href="">CANCELLATION</a><br>
+                <a href="">RETURN</a><br>
+                <a href="">CAREER</a><br>
+              </div>
+            </div>
+        </div>
+        <div class="col-lg-3 text-center pt-3 pl">
+            <div class="desktopshoplink">
+               <div class="desktoptitle">
+                <a href=""><h4>KEEP IN TOUCH</h4></a>
+                </div>
+              <div class="">
+                    <a href="" target="_blank"><i class="fab fa-2x fa-instagram" style="color:red; opacity: .7;"></i></a><br>
+                    <a href="" target="_blank"><i class="fab  fa-2x fa-twitter" style="color: #00acee"></i></a><br>
+                    <a href="" target="_blank"><i class="fab  fa-2x fa-facebook" style="color: #3b5998"></i></a><br>
+                    <a href="" target="_blank"><i class="fab  fa-2x fa-youtube" style="color: #c4302b"></i></a>
+              </div>
+            </div>
+          
+        </div>
+        <div class="col-lg-3 pt-3 text-center">
+          <div class="footerlastcol">
+
+             <h4> <strong>100% ORIGINAL PRODUCTS</strong></h4>
+              <p>Guarantee for all product at indianhandicraft.com</p>
+             <h4><strong><i class="fas fa-lg fa-exchange-alt pr-3"></i>Return with in 30 days</strong></h4> <p>Of Receiving Your Order</p>
+
+             <h4><strong><i class="fas fa-lg fa-truck pr-3"></i>Get Free Delivery</strong></h4>
+             <p>For Every Order Above Rs....</p>
+
+            
+          </div>
+          
+        </div>
+
+      </div>
+      <div class="row">
+          
+       <!--    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13259.58967991885!2d150.99997798331168!3d-33.81496019073664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12a318e167af4f%3A0x5017d681632c600!2sParramatta+NSW+2150%2C+Australia!5e0!3m2!1sen!2sin!4v1562558237640!5m2!1sen!2sin" width="100%" height="45%" frameborder="0" style="border:0" allowfullscreen></iframe> -->
+         
+
+      </div>
+    </div>
+</footer>
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
